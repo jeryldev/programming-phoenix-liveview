a784a3f504acc1469496226adaed8a5e6a504cfa
la-jeryl <jeryl.estopace@lawadvisor.com>
2022-11-23 18:31:51 +0800

    Build the Live Demographic Form Component

--- pento/lib/pento_web/live/survey_live.ex
+++ pento/lib/pento_web/live/survey_live.ex
@@ -2,6 +2,7 @@ defmodule PentoWeb.SurveyLive do
   use PentoWeb, :live_view
   alias PentoWeb.DemographicLive
   alias Pento.Survey
+  # alias PentoWeb.DemographicLive.Form
   # alias __MODULE__.Component
 
   def mount(_params, _session, socket) do
